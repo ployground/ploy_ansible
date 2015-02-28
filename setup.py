23bf54679f4d4b99896dda7f2c7b6d8131d4025d
Florian Schulze <florian.schulze@gmx.net>
2015-02-28 20:30:54 +0100

    Bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 HISTORY = open(os.path.join(here, 'HISTORY.rst')).read()
 
 
-version = "1.2.3"
+version = "1.2.4.dev0"
 
 
 install_requires = [
